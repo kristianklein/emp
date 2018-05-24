09444fe3ed19d8d07fdfc8198cf0a75ebd6009dd
Kristian Klein <kristian.klein90@gmail.com>
2018-05-24 11:10:40 +0200

    Initial commit

---
+++ Debug/include/subdir_vars.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../include/button.c \
+../include/keypad.c \
+../include/launchpad.c \
+../include/lcd.c \
+../include/rtc.c \
+../include/uart0.c 
+
+C_DEPS += \
+./include/button.d \
+./include/keypad.d \
+./include/launchpad.d \
+./include/lcd.d \
+./include/rtc.d \
+./include/uart0.d 
+
+OBJS += \
+./include/button.o \
+./include/keypad.o \
+./include/launchpad.o \
+./include/lcd.o \
+./include/rtc.o \
+./include/uart0.o 
+
+OBJS__QUOTED += \
+"include\button.o" \
+"include\keypad.o" \
+"include\launchpad.o" \
+"include\lcd.o" \
+"include\rtc.o" \
+"include\uart0.o" 
+
+C_DEPS__QUOTED += \
+"include\button.d" \
+"include\keypad.d" \
+"include\launchpad.d" \
+"include\lcd.d" \
+"include\rtc.d" \
+"include\uart0.d" 
+
+C_SRCS__QUOTED += \
+"../include/button.c" \
+"../include/keypad.c" \
+"../include/launchpad.c" \
+"../include/lcd.c" \
+"../include/rtc.c" \
+"../include/uart0.c" 
+
+
