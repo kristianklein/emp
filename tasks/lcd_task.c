7e3fa56ab15f2ada3fcec8722f089b6ba79a7390
Anders Sørensen <anders1222@gmail.com>
2018-05-25 10:36:48 +0200

    Move lcd_task.c to correct folder

---
+++ tasks/lcd_task.c
@@ -0,0 +1,61 @@
+/*
+ * lcd_task.c
+ *
+ *  Created on: 24. maj 2018
+ *      Author: Anders Sørensen
+ */
+
+/***************************** Included files ********************************/
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "emp_type.h"
+#include "FreeRTOS.h"
+#include "lcd.h"
+#include "task.h"
+#include "queue.h"
+
+/***************************** Defines ***************************************/
+#define WAIT_FOR_POWER_ON           3       //Important! if the time slice size is changed, then change this number
+
+
+
+/***************************** Constants *************************************/
+/***************************** Variables *************************************/
+
+/***************************** Helper Functions decelerations ****************/
+
+/***************************** Functions *************************************/
+void lcd_task(void *pvParameters)
+{
+    extern xQueueHandle xQueueLcd;
+    INT8U char_value;
+
+    lcd_init();
+
+    while(1)
+    {
+        xQueueReceive(xQueueLcd,(void *) &char_value, 5);
+
+        if(char_value == 0x0A)
+        {
+            //Make a new line
+        }
+        else if(char_value  == 0x00)
+        {
+            lcd_clear();
+        }
+        else
+        {
+            lcd_write(char_value);
+        }
+    }
+    return;
+}
+
+
+
+
+
+
+
+
